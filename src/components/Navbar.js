baf6f4ecbe7fe244c09c5d0091c5379ce14b28a7
Raj Patel <raj191@live.com>
2021-08-26 22:01:19 -0400

    adding a navbar to the page

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (  
+    <ul>
+    <li ><a class="active" href="#about">NBA LOOKUP</a></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#news">About</a></li>
+        <li><a href="#contact">Code</a></li>
+        
+    </ul>
+    );
+}
+ 
+export default Navbar;
