c255dbc00e5ed5414873757b8993144368112f1e
RajPatel <raj191@live.com>
2021-09-09 01:02:22 -0400

    adding stats call

--- src/components/displays/PlayerDisplay.js
+++ src/components/displays/PlayerDisplay.js
@@ -1,23 +1,46 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import useFeach from "../hooks/useFeach";
+import {useFeach} from "../hooks/useFeach";
 
 const PlayerDisplay = (props) => {
     const [currentPlayer, setCurrentPlayer] = useState({id:237,first_name:"LeBron",height_feet:6,height_inches:8,last_name:"James",position:"F",team:{id:14,abbreviation:"LAL",city:"Los Angeles",conference:"West",division:"Pacific",full_name:"Los Angeles Lakers",name:"Lakers"},weight_pounds:250});
     const [playerTeam, setPlayerTeam] = useState('');
     const [isPending, setIsPending] =useState(true);
     const [error, setError] = useState(null);
+    const [seasonData, setSeasonData] = useState(null);
     const  searchInput = props.searchInput;
     //const defaultURL = 'https://www.balldontlie.io/api/v1/players/237';
     const defaultURL =   `https://www.balldontlie.io/api/v1/players?search=${searchInput}`
 
-    //grabbing data when searchbox is used
-    useEffect(()=>{
-        console.log(currentPlayer)
+    //https://www.balldontlie.io/api/v1/season_averages?season=2020&player_ids[]=237  link to get the season stats of a player. just replace the season desired and the player id
+
+
+    const getStats = (num)=>{
+        let statsCall = `https://www.balldontlie.io/api/v1/season_averages?season=2020&player_ids[]=${num}`
         fetch(defaultURL)
         .then(res => {
             if (!res.ok){
-                throw Error("Could now get the data");
+                throw Error("Could not get the stats");
+            }
+            return res.json();
+        })
+        .then(data => {
+            console.log(data)
+            setSeasonData(data)
+            setIsPending(false);
+        })
+        .catch(err=>{
+            console.log(err.message);
+        })
+
+    }
+
+    //grabbing data when searchbox is used
+    useEffect(()=>{
+        fetch(defaultURL)
+        .then(res => {
+            if (!res.ok){
+                throw Error("Could not get the data");
             }
             return res.json();
         })
@@ -29,6 +52,11 @@ const PlayerDisplay = (props) => {
         .catch(err=>{
             console.log(err.message);
         })
+        if(currentPlayer){
+            console.log(currentPlayer.id)
+            getStats(currentPlayer.id);
+        }
+        
 
     },[searchInput])
 
@@ -39,6 +67,7 @@ const PlayerDisplay = (props) => {
         {currentPlayer && <p>Position: {currentPlayer.position}</p>}
         {currentPlayer && <p>Height: {currentPlayer.height_feet+" ft, "+currentPlayer.height_inches+" in"}</p>}
         {currentPlayer && <p>Weight: {currentPlayer.weight_pounds} lbs</p>}
+        {console.log(seasonData)}
         
         </div>
      );
