7a06b742a480faef412f0d9e9e48bfa3d259950e
Raj Patel <raj191@live.com>
2021-09-11 15:23:00 -0400

    adding display output for teams

--- src/components/displays/TeamDisplay.js
+++ src/components/displays/TeamDisplay.js
@@ -1,11 +1,42 @@
-
+import { useEffect } from "react";
+import { useState } from "react";
 
 const TeamDisplay = (props) => {
     const searchInput = props.searchInput;
+    const [teams, setTeams] = useState(null)
+
+    useEffect(()=>{
+        let url = 'https://www.balldontlie.io/api/v1/teams'
+        fetch(url)
+        .then(res => {
+            if (!res.ok){
+                throw Error("Could now get the data");
+            }
+            return res.json();
+        })
+        .then(data => {
+            setTeams(data.data);
+        })
+        .catch(err=>{
+            console.log(err.message);
+        })
+
+    },[searchInput])
     
     return ( 
         <div className="teamDisplay-wrapper">
-        {console.log('teamDisplay')}
+        {console.log(teams)}
+        {teams && teams.map((tms)=>(
+            <div className="list-wrapper" key={tms.id}>
+            <h4 className="team-name">{tms.full_name}</h4>
+            <p>Conference: {tms.conference}</p>
+            <p>Division: {tms.division}</p>
+            <p>Abbreviation: {tms.abbreviation}</p>
+            <p>City: {tms.city}</p>
+
+            </div>
+        ))}
+
         </div>
      );
 }
