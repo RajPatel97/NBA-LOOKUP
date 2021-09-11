93847716962bf63d9c6fcd8d92d254e57a997b75
RajPatel <raj191@live.com>
2021-09-10 21:08:57 -0400

    fixing some bugs

--- src/components/displays/PlayerDisplay.js
+++ src/components/displays/PlayerDisplay.js
@@ -3,16 +3,9 @@ import { useState } from "react";
 import {useFeach} from "../hooks/useFeach";
 
 const PlayerDisplay = (props) => {
-    const [currentPlayer, setCurrentPlayer] = useState({id:237,first_name:"LeBron",height_feet:6,height_inches:8,last_name:"James",position:"F",team:{id:14,abbreviation:"LAL",city:"Los Angeles",conference:"West",division:"Pacific",full_name:"Los Angeles Lakers",name:"Lakers"},weight_pounds:250});
-    const [playerTeam, setPlayerTeam] = useState('');
-    const [isPending, setIsPending] =useState(true);
-    const [error, setError] = useState(null);
+    const [currentPlayer, setCurrentPlayer] = useState(null);
     const [seasonData, setSeasonData] = useState(null);
     const  searchInput = props.searchInput;
-    //const defaultURL = 'https://www.balldontlie.io/api/v1/players/237';
-    const defaultURL =   `https://www.balldontlie.io/api/v1/players?search=${searchInput}`
-
-    //https://www.balldontlie.io/api/v1/season_averages?season=2020&player_ids[]=237  link to get the season stats of a player. just replace the season desired and the player id
 
 
     const getStats = (num)=>{
@@ -27,7 +20,6 @@ const PlayerDisplay = (props) => {
         .then(data => {
             //console.log(data)
             setSeasonData(data.data[0])
-            setIsPending(false);
         })
         .catch(err=>{
             console.log(err.message);
@@ -37,6 +29,7 @@ const PlayerDisplay = (props) => {
 
     //grabbing data when searchbox is used
     useEffect(()=>{
+        const defaultURL =   `https://www.balldontlie.io/api/v1/players?search=${searchInput}`
         fetch(defaultURL)
         .then(res => {
             if (!res.ok){
@@ -45,34 +38,30 @@ const PlayerDisplay = (props) => {
             return res.json();
         })
         .then(data => {
-            //setData(data);
             setCurrentPlayer(data.data[0])
-            setIsPending(false);
+            getStats(data.data[0].id);
         })
         .catch(err=>{
             console.log(err.message);
         })
-        if(currentPlayer){
-            getStats(currentPlayer.id);
-        }
+        
         
 
-    },[searchInput])
+    },[searchInput])//rerendering when there is a new search here 
 
     return ( 
         <div className="playerDisplay-wrapper">
         {currentPlayer && <h1 className="name">{currentPlayer.first_name +" "+ currentPlayer.last_name}</h1>}
-        {currentPlayer && <h2 className="team-name">{currentPlayer.team.full_name}</h2>}{/*have to load this way or error will show on first render */}
+        {currentPlayer && <h2 className="team-name">{currentPlayer.team.full_name}</h2>}{/*have to load this way or error will show on first render and while loading the data */}
         {currentPlayer && <p>Position: {currentPlayer.position}</p>}
         {currentPlayer && <p>Height: {currentPlayer.height_feet+" ft, "+currentPlayer.height_inches+" in"}</p>}
         {currentPlayer && <p>Weight: {currentPlayer.weight_pounds} lbs</p>}
-        <h3>Stats for the {seasonData.season} season:</h3>
-        <p>PPG: {seasonData.pts}</p>
-        <p>AST: {seasonData.ast}</p>
-        <p>REB: {seasonData.reb}</p>
-        <p>Games Played: {seasonData.games_played}</p>
-        <p>Minutes per game: {seasonData.min}</p>
-        {console.log(seasonData)}
+        {seasonData && <h3>Stats for the {seasonData.season} season:</h3>}
+        {seasonData && <p>PPG: {seasonData.pts}</p>}
+        {seasonData &&  <p>AST: {seasonData.ast}</p>}
+        {seasonData &&  <p>REB: {seasonData.reb}</p>}
+        {seasonData &&  <p>Games Played: {seasonData.games_played}</p>}
+        {seasonData &&  <p>Minutes per game: {seasonData.min}</p>}
         
         </div>
      );
